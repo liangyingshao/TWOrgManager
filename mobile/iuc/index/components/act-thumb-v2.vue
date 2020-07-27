0aba82a47552239a2d061e964e7aba4e8bf1fd0a
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-07-27 13:23:25 +0800

    更新学生首页

--- mobile/iuc/index/components/act-thumb-v2.vue
+++ mobile/iuc/index/components/act-thumb-v2.vue
@@ -1,20 +1,20 @@
 <template>
 	<view class="act-main" @click="toDetail()">
 		<view class="title">
-			<view class="text-cut">活动的正式名称，这里可以显示非常长的名字，不用处理截断</view>
+			<view class="text-cut">{{activity.ActivityName}}</view>
 		</view>
 		<view class="content margin-bottom-sm">
 			<image src="https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg"
 			 mode="aspectFill"></image>
 			<view class="desc">
 				<view class="text-content"> 
-					活动的简介！这里可能是一个非常非常长的简介，但是理论上这里的内容不应该超过2行。总高度是自动的。
+					{{activity.Description ? activity.Description : "暂无简介"}}
 				</view>
 				<view>
-					<view class="cu-tag sm round">2020年10月10日 20时20分</view>
+					<view class="cu-tag sm round">{{activity.StartDate}}</view>
 				</view>
 				<view>
-					<view class="cu-tag sm round">厦门大学海韵园实验楼2-202</view>
+					<view class="cu-tag sm round">{{activity.Address ? activity.Address : "暂无地址"}}</view>
 				</view>
 			</view>
 		</view>
@@ -29,6 +29,9 @@
 					url: "/iuc/activity/activity-detail"
 				})
 			}
+		},
+		props:{
+			activity: Object
 		},
 		data () {
 			return {};
---
+++ mobile/iuc/orgmanagement/orgList.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<cu-custom isBack bgColor="bg-gradual-blue">
+			<block slot="content">社团列表</block>
+		</cu-custom>
+		<view class="cu-list menu">
+			<org-info v-for="org in orgs" :key="org.ID" :orgInfo="org"></org-info>
+		</view>
+	</view>
+</template>
+
+<script>
+	import orgInfo from '../index/components/org-info-simple.vue'
+	export default {
+		components: {
+			orgInfo
+		},
+		methods: {
+			getAllOrg() {
+				uni.post("/api/security/GetAllDeparts", {}, msg => {
+					if (msg.success) {
+						this.orgs = msg.data;
+					}
+				})
+			}
+		},
+		data() {
+			return {
+				orgs: []
+			}
+		},
+		onLoad() {
+			this.getAllOrg();
+		}
+	}
+</script>
+
+<style>
+</style>
