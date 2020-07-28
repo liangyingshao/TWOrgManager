7653d4da825a8c06445fcab96725f1ed9b90a79a
yuehaoa <53177683+yuehaoa@users.noreply.github.com>
2020-07-28 23:04:27 +0800

    增加挂靠单位管理员页面和管理员选择管理社团页面

---
+++ mobile/iuc/index/manager-depart-choose.vue
@@ -0,0 +1,62 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-blue" isBack>
+			<block slot="backText">返回</block>
+		</cu-custom>
+		<view>
+			<view class="cu-bar bg-white solids-bottom">
+				<view class="action">
+					<text class="cuIcon-titles text-blue"></text>
+					<text class="block position-relative">
+						社团列表
+						<view class='cu-tag bg-red margin-left-sm round' v-if="departs">{{departs.length}}</view>
+					</text>
+				</view>
+			</view>
+		<view class="padding bg-white solids-bottom" v-for="(item,index) in departs" :key="index" @click="toDetail(item.ID)">
+			<view class="cu-item">
+				<view class="content padding-tb-sm">
+				  <view>
+					<text class="cuIcon-newsfill text-blue margin-right-xs"></text>社团名称: {{item.Name}}</view>
+					<view class="text-gray text-sm text-cut margin-top-sm" v-if="item.Description!==''">
+						<text class="cuIcon-wenzi margin-right-xs"></text>社团简介：{{item.Description}}
+					</view>
+					<view class="margin-top-sm" v-if="item.Description==''">
+						<text class="cuIcon-wenzi margin-right-xs"></text>暂无简介
+					</view>
+				</view>
+				
+			  </view>
+		  </view>
+		</view>
+	</view>
+</template>
+
+<script>
+	let app = require("@/config");
+	export default{
+		data() {
+			return{
+				departs:[]
+			}
+		},
+		onLoad() {
+		  uni.post("/api/security/GetAllDeparts", {}, msg => {
+			this.departs = msg.data;
+		  })
+		},
+		methods: {
+			toDetail(departId) {
+				uni.chosePostion(departId);
+				console.log(app.defaultDepartId);
+				uni.navigateTo({
+				  url: "/iuc/index/index-manager"
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
+
