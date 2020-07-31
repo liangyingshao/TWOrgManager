908a9f05746fefc99e1b58c3b4a2fe6cec5ef0eb
liangyingshao <1669807584@qq.com>
2020-07-31 10:41:16 +0800

    v-model

--- mobile/iuc/activity/activity.vue
+++ mobile/iuc/activity/activity.vue
@@ -13,51 +13,51 @@
 				</view>
 				<view class="cu-form-group">
 					<view class="title">社团名称</view>
-					<input name="DepartName" :value="io.data.DepartName" :disabled="io.fieldAccess.DepartName !== 'w' || !io.isMyStep"></input>
+					<input name="DepartName" v-model="io.data.DepartName" :disabled="io.fieldAccess.DepartName !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">活动名称</view>
-					<input name="ActivityName" :value="io.data.ActivityName" :disabled="io.fieldAccess.ActivityName !== 'w' || !io.isMyStep"></input>
+					<input name="ActivityName" v-model="io.data.ActivityName" :disabled="io.fieldAccess.ActivityName !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">活动时间</view>
-					<input name="StartDate" :value="io.data.StartDate" :disabled="io.fieldAccess.StartDate !== 'w' || !io.isMyStep"></input>
+					<input name="StartDate" v-model="io.data.StartDate" :disabled="io.fieldAccess.StartDate !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">活动时间</view>
-					<input name="EndDate" :value="io.data.EndDate" :disabled="io.fieldAccess.EndDate !== 'w' || !io.isMyStep"></input>
+					<input name="EndDate" v-model="io.data.EndDate" :disabled="io.fieldAccess.EndDate !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">活动人数</view>
-					<input name="AttendanceFigures" :value="io.data.AttendanceFigures" :disabled="io.fieldAccess.AttendanceFigures !== 'w' || !io.isMyStep"></input>
+					<input name="AttendanceFigures" v-model="io.data.AttendanceFigures" :disabled="io.fieldAccess.AttendanceFigures !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">预算金额</view>
-					<input name="Budget" :value="io.data.Budget" :disabled="io.fieldAccess.Budget !== 'w' || !io.isMyStep"></input>
+					<input name="Budget" v-model="io.data.Budget" :disabled="io.fieldAccess.Budget !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">活动地点</view>
-					<input name="Address" :value="io.data.Address" :disabled="io.fieldAccess.Address !== 'w' || !io.isMyStep"></input>
+					<input name="Address" v-model="io.data.Address" :disabled="io.fieldAccess.Address !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">负责人员</view>
-					<input name="Leader" :value="io.data.Leader" :disabled="io.fieldAccess.Leader !== 'w' || !io.isMyStep"></input>
+					<input name="Leader" v-model="io.data.Leader" :disabled="io.fieldAccess.Leader !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">联系方式</view>
-					<input name="Telephone" :value="io.data.Telephone" :disabled="io.fieldAccess.Telephone !== 'w' || !io.isMyStep"></input>
+					<input name="Telephone" v-model="io.data.Telephone" :disabled="io.fieldAccess.Telephone !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">活动类型</view>
-					<input name="ActivityType" :value="io.data.ActivityType" :disabled="io.fieldAccess.ActivityType !== 'w' || !io.isMyStep"></input>
+					<input name="ActivityType" v-model="io.data.ActivityType" :disabled="io.fieldAccess.ActivityType !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">面向范围</view>
-					<input name="FaceTo" :value="io.data.FaceTo" :disabled="io.fieldAccess.FaceTo !== 'w' || !io.isMyStep"></input>
+					<input name="FaceTo" v-model="io.data.FaceTo" :disabled="io.fieldAccess.FaceTo !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group">
 					<view class="title">活动内容</view>
-					<input name="Description" :value="io.data.Description" :disabled="io.fieldAccess.Description !== 'w' || !io.isMyStep"></input>
+					<input name="Description" v-model="io.data.Description" :disabled="io.fieldAccess.Description !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-bar bg-white solids-bottom margin-top-xs" v-if="io.fieldAccess.GuideTeacherOpinion">
 					<view class="action">
@@ -79,15 +79,15 @@
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.GuideTeacherOpinion">
 					<view class="title">审核意见</view>
-					<input name="GuideTeacherOpinion" :value="io.data.GuideTeacherOpinion" :disabled="io.fieldAccess.GuideTeacherOpinion !== 'w' || !io.isMyStep"></input>
+					<input name="GuideTeacherOpinion" v-model="io.data.GuideTeacherOpinion" :disabled="io.fieldAccess.GuideTeacherOpinion !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.GuideTeacherOpinion">
 					<view class="title">教师姓名</view>
-					<input name="GuideTeacher" :value="io.data.GuideTeacher" :disabled="io.fieldAccess.GuideTeacher !== 'w' || !io.isMyStep"></input>
+					<input name="GuideTeacher" v-model="io.data.GuideTeacher" :disabled="io.fieldAccess.GuideTeacher !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.GuideTeacherOpinion">
 					<view class="title">审核时间</view>
-					<input name="GuideTeacherTime" :value="io.data.GuideTeacherTime" :disabled="io.fieldAccess.GuideTeacherTime !== 'w' || !io.isMyStep"></input>
+					<input name="GuideTeacherTime" v-model="io.data.GuideTeacherTime" :disabled="io.fieldAccess.GuideTeacherTime !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-bar bg-white solids-bottom margin-top-xs" v-if="io.fieldAccess.AffiliatedDepartOpinion">
 					<view class="action">
@@ -109,15 +109,15 @@
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.AffiliatedDepartOpinion">
 					<view class="title">审核意见</view>
-					<input name="AffiliatedDepartOpinion" :value="io.data.AffiliatedDepartOpinion" :disabled="io.fieldAccess.AffiliatedDepartOpinion !== 'w' || !io.isMyStep"></input>
+					<input name="AffiliatedDepartOpinion" v-model="io.data.AffiliatedDepartOpinion" :disabled="io.fieldAccess.AffiliatedDepartOpinion !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.AffiliatedDepartOpinion">
 					<view class="title">审核人员</view>
-					<input name="AffiliatedDepart" :value="io.data.AffiliatedDepart" :disabled="io.fieldAccess.AffiliatedDepart !== 'w' || !io.isMyStep"></input>
+					<input name="AffiliatedDepart" v-model="io.data.AffiliatedDepart" :disabled="io.fieldAccess.AffiliatedDepart !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.AffiliatedDepartOpinion">
 					<view class="title">审核时间</view>
-					<input name="AffiliatedDepartTime" :value="io.data.AffiliatedDepartTime" :disabled="io.fieldAccess.AffiliatedDepartTime !== 'w' || !io.isMyStep"></input>
+					<input name="AffiliatedDepartTime" v-model="io.data.AffiliatedDepartTime" :disabled="io.fieldAccess.AffiliatedDepartTime !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-bar bg-white solids-bottom margin-top-xs" v-if="io.fieldAccess.SauOpinion">
 					<view class="action">
@@ -137,15 +137,15 @@
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.SauOpinion">
 					<view class="title">审核意见</view>
-					<input name="SauOpinion" :value="io.data.SauOpinion" :disabled="io.fieldAccess.SauOpinion !== 'w' || !io.isMyStep"></input>
+					<input name="SauOpinion" v-model="io.data.SauOpinion" :disabled="io.fieldAccess.SauOpinion !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.SauOpinion">
 					<view class="title">审核人员</view>
-					<input name="Sau" :value="io.data.Sau" :disabled="io.fieldAccess.Sau !== 'w' || !io.isMyStep"></input>
+					<input name="Sau" v-model="io.data.Sau" :disabled="io.fieldAccess.Sau !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.SauOpinion">
 					<view class="title">审核时间</view>
-					<input name="SauTime" :value="io.data.SauTime" :disabled="io.fieldAccess.SauTime !== 'w' || !io.isMyStep"></input>
+					<input name="SauTime" v-model="io.data.SauTime" :disabled="io.fieldAccess.SauTime !== 'w' || !io.isMyStep"></input>
 				</view>
 				<view class="cu-bar bg-white solids-bottom margin-top-xs" v-if="io.fieldAccess.YlcOpinion">
 					<view class="action">
@@ -167,15 +167,15 @@
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.YlcOpinion">
 					<view class="title">审核意见</view>
-					<input name="YlcOpinion" :value="io.data.YlcOpinion" :disabled="io.fieldAccess.YlcOpinion !== 'w' || !io.YlcOpinion"></input>
+					<input name="YlcOpinion" v-model="io.data.YlcOpinion" :disabled="io.fieldAccess.YlcOpinion !== 'w' || !io.YlcOpinion"></input>
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.YlcOpinion">
 					<view class="title">审核人员</view>
-					<input name="Ylc" :value="io.data.Ylc" :disabled="io.fieldAccess.Ylc !== 'w' || !io.Ylc"></input>
+					<input name="Ylc" v-model="io.data.Ylc" :disabled="io.fieldAccess.Ylc !== 'w' || !io.Ylc"></input>
 				</view>
 				<view class="cu-form-group" v-if="io.fieldAccess.YlcOpinion">
 					<view class="title">审核时间</view>
-					<input name="YlcTime" :value="io.data.YlcTime" :disabled="io.fieldAccess.YlcTime !== 'w' || !io.YlcTime"></input>
+					<input name="YlcTime" v-model="io.data.YlcTime" :disabled="io.fieldAccess.YlcTime !== 'w' || !io.YlcTime"></input>
 				</view>
 				<view class="flex flex-direction margin-tb" v-show="io.isMyStep">
 					<button class="cu-btn bg-blue lg" form-type="submit">提交</button>
@@ -237,7 +237,7 @@
 				});
 			},
 			submit(e) {
-				this.io.data = e.detail.value;
+				//this.io.data = e.detail.value;
 				this.io.shouldUpload.forEach(value => {
 					this.upLoad[value] = this.io[value] || this.io.data[value]
 				});
