e57b529c9ec7d10991d3b844ebde57b0a4b892b3
yuehaoa <53177683+yuehaoa@users.noreply.github.com>
2020-10-23 14:59:45 +0800

    增加手机版个人中心字段

--- mobile/iuc/profile/userInfo.vue
+++ mobile/iuc/profile/userInfo.vue
@@ -16,8 +16,48 @@
 				<input v-model="userInfo.RealName" placeholder="请输入姓名" name="input" class="text-right"></input>
 			</view>
 			<view class="cu-form-group">
+				<view class="title">民族</view>
+				<input v-model="userInfo.BirthPlace" placeholder="请输入民族" name="input" class="text-right"></input>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">性别</view>
+				<picker @change="PickerChange($event, 'Gender')" :value="genIndex" :range="gender">
+					<view class="picker">
+						{{userInfo.Gender === '' ? gender[0] : userInfo.Gender}}
+					</view>
+				</picker>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">籍贯</view>
+				<input v-model="userInfo.Source" placeholder="请输入籍贯" name="input" class="text-right"></input>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">学历</view>
+				<picker @change="PickerChange($event, 'Educational')" :value="eduIndex" :range="dic['学历']">
+					<view class="picker">
+						{{userInfo.Educational === '' ? dic['学历'][0] : userInfo.Educational}}
+					</view>
+				</picker>
+			</view>
+			<view class="cu-form-group">
 				<view class="title">学/工号</view>
 				<input :disabled="true" v-model="userInfo.Code" name="input" class="text-right"></input>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">所属校区</view>
+				<picker @change="PickerChange($event, 'Campus')" :value="campIndex" :range="dic['校区']">
+					<view class="picker">
+						{{userInfo.Campus === '' ? dic["校区"][0]:userInfo.Campus}}
+					</view>
+				</picker>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">学院</view>
+				<picker @change="PickerChange($event, 'Depart')" :value="belongIndex" :range="dic['学院']">
+					<view class="picker">
+						{{userInfo.BelongDepart === '' ? dic["学院"][0]:userInfo.BelongDepart}}
+					</view>
+				</picker>
 			</view>
 			<view class="cu-form-group">
 				<view class="title">手机</view>
@@ -26,6 +66,10 @@
 			<view class="cu-form-group">
 				<view class="title">邮箱</view>
 				<input placeholder="请输入邮箱" v-model="userInfo.Email" name="input" class="text-right"></input>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">QQ</view>
+				<input placeholder="请输入QQ" v-model="userInfo.QQ" name="input" class="text-right"></input>
 			</view>
 			<view class="cu-form-group">
 				<view class="title">入团时间</view>
@@ -43,6 +87,14 @@
 					</view>
 				</picker>
 			</view>
+			<view class="cu-form-group">
+				<view class="title">政治面貌</view>
+				<picker @change="PickerChange($event, 'Political')" :value="poliIndex" :range="dic['政治面貌']">
+					<view class="picker">
+						{{userInfo.PoliticalStatus === '' ? dic["政治面貌"][0]:userInfo.PoliticalStatus}}
+					</view>
+				</picker>
+			</view>
 		</form>
 		<button class="margin-top bg-blue lg margin-lr" form-type="submit" @click="save()">确认更新</button>
 	</view>
@@ -52,17 +104,65 @@
 	export default{
 		onShow() {
 			this.GetUserInfo();
+			this.GetDataDictionary();
 		},
 		data() {
 			return{
-				userInfo: {}
+				userInfo: {},
+				dic: [],
+				campIndex: -1,
+				genIndex: -1,
+				eduIndex: -1,
+				belongIndex: -1,
+				souIndex: -1,
+				poliIndex: -1,
+				gender: ["男", "女"]
 			}
 		},
 		methods: {
+			PickerChange(e, option){
+				if(option === 'Political'){
+					this.userInfo.PoliticalStatus = this.dic['政治面貌'][e.detail.value];
+					this.poliIndex = e.detail.value;
+				}
+				else if(option === 'Campus') {
+					this.userInfo.Campus = this.dic["校区"][e.detail.value];
+					this.campIndex = e.detail.value;
+				}
+				else if(option === 'Educational'){
+					this.userInfo.Educational = this.dic['学历'][e.detail.value];
+					this.eduIndex = e.detail.value;
+				}
+				else if(option === 'Depart') {
+					this.userInfo.BelongDepart = this.dic['学院'][e.detail.value];
+					this.belongIndex = e.detail.value;
+				}
+				else if(option === 'gender') {
+					this.userInfo.Gender = this.gender[e.detail.value];
+					this.genIndex = e.detail.value;
+				}
+			},
 			GetUserInfo() {
 				uni.post("/api/security/GetUserById", {}, msg=>{
 					if (msg.success) {
 						this.userInfo = msg.user;
+					}
+				})
+			},
+			GetDataDictionary() {
+				uni.post("/api/config/GetDataDictionary", {}, msg=>{
+					if (msg.success) {
+						var x = {};
+						let t = msg.Dictionary;
+						for(let obj in t) {
+							var temp = [];
+							for(let i  in t[obj])
+							{
+								temp.push(t[obj][i]);
+							}
+							x[obj] = temp;
+						}
+						this.dic = x;
 					}
 				})
 			},
